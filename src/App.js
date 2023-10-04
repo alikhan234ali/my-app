363a853e5dcb73c95e4e97214b8e1d896760167c
Ali Ahmad <alikhanhi912@gmail.com>
2023-10-04 02:37:38 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-heade">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="main">
+      <div class="profile-img" ><img src="profile-img.jpg"/></div>
+      <br/> <br/>
+      <header>
+        <h1>Ali Ahmad</h1>
+        <p>Web Developer</p>
       </header>
+        
+      <section class="contact">
+        <h2 class="contact1">Contact Information</h2>
+        <address>
+          <p>Email: alikanhi912@gmail.com</p>
+          <p>Phone: (+92) 306-4382254</p>
+          <p>Address: Luddan, Panjab, Pakistan</p>
+        </address>
+      </section>
+
+      <section class="education">
+        <h2>B'S Information Technologies</h2>
+        <ul>
+          <li>
+            <h3>Bachelor of Science in Computer Science</h3>
+            <p>University of education, 2020 to 2024</p>
+          </li>
+        </ul>
+      </section>
+
+      <section class="experience">
+        <h2>Work Experience</h2>
+        <ul>
+          <li>
+            <h3>Web Developer</h3>
+            <h3>Wordpress</h3>
+            <p>LinkedIn, 2022</p>
+            <p>Guru.com, 2021</p>
+            <ul>
+              <li>Developed and maintained web applications</li>
+              <li>Collaborated with cross-functional teams</li>
+            </ul>
+          </li>
+        </ul>
+      </section>
+
+      <section class="skills">
+        <h2>Skills</h2>
+        <ul>
+          <li>HTML, CSS, JavaScript</li>
+          <li>Front-end Frameworks (e.g., React, Vue)</li>
+          <li>Back-end Technologies (e.g., Node.js, PHP)</li>
+          <li>Database Management (e.g., SQL, MongoDB)</li>
+          <li>Wordpress </li>
+        </ul>
+      </section>
+
+      <section class="languages">
+        <h2>Languages</h2>
+        <ul>
+          <li>English (Fluent)</li>
+          <li>Spanish (Intermediate)</li>
+          <li>Urdu</li>
+        </ul>
+      </section>
+
+      <footer>
+        <p>&copy; 2023 Ali Ahmad</p>
+      </footer>
     </div>
   );
 }
