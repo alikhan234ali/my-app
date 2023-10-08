a1a6068367d74f61593e3d5e3110b438d0d1f8aa
Ali Ahmad <alikhanhi912@gmail.com>
2023-10-07 23:11:12 -0700

    update

--- src/ali.js
+++ src/ali.js
@@ -3,17 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="main">
-      <div className="main2">
-      <div className="profile-img" ><img src="profile-img.jpg"/></div>
+    <div class="main">
+      <div class="main2">
+      <div class="profile-img" ><img src="profile-img.jpg"/></div>
       <br/> <br/>
       <header>
         <h1>Ali Ahmad</h1>
         <p>Web Developer</p>
       </header>
         
-      <section className="contact">
-        <h2 className="contact1">Contact Information</h2>
+      <section class="contact">
+        <h2 class="contact1">Contact Information</h2>
         <address>
           <p>Email: alikanhi912@gmail.com</p>
           <p>Phone: (+92) 306-4382254</p>
@@ -21,7 +21,7 @@ function App() {
         </address>
       </section>
 
-      <section className="education">
+      <section class="education">
         <h2>B'S Information Technologies</h2>
         <ul>
           <li>
@@ -31,7 +31,7 @@ function App() {
         </ul>
       </section>
 
-      <section className="experience">
+      <section class="experience">
         <h2>Work Experience</h2>
         <ul>
           <li>
@@ -47,7 +47,7 @@ function App() {
         </ul>
       </section>
 
-      <section className="skills">
+      <section class="skills">
         <h2>Skills</h2>
         <ul>
           <li>HTML, CSS, JavaScript</li>
@@ -58,7 +58,7 @@ function App() {
         </ul>
       </section>
 
-      <section className="languages">
+      <section class="languages">
         <h2>Languages</h2>
         <ul>
           <li>English (Fluent)</li>
